ec390317e0f395c1742acb435a7e1b5b7717da08
yuliapro <julia.konyushina@gmail.com>
2026-03-23 20:18:09 +0100

    Update solow.sql

--- solow/solow.sql
+++ solow/solow.sql
@@ -1,18 +1,18 @@
 /* TEMPLATE: Universal Traffic & Behavior Analysis
 Instructions: 
-1. Replace 'user_metrics' with your source table.
-2. Replace 'labor_minutes' with the metric you want to analyze (e.g., sessions, spend).
-3. Replace 'user_level_max' with your grouping dimension (e.g., campaign_id, country).
+1. Replace 'Datawarehouse.gold.user_zscore_segmentation' with your source table.
+2. Replace 'duration_seconds' with the metric you want to analyze (e.g., sessions, spend).
+3. Replace 'max_level_reached' with your grouping dimension (e.g., campaign_id, country).
 */
 
 WITH base_data AS (
     -- Step 1: Define your metric and dimension here
     SELECT 
-        user_level_max AS grouping_id, 
-        labor_minutes  AS metric_value,
-        COUNT(*) OVER(PARTITION BY user_level_max) AS total_count,
-        AVG(labor_minutes) OVER(PARTITION BY user_level_max) AS avg_value
-    FROM user_metrics
+        max_level_reached AS grouping_id, 
+        CAST(duration_seconds AS FLOAT)  AS metric_value,
+        CAST(COUNT(*)  OVER(PARTITION BY max_level_reached)AS FLOAT) AS total_count,
+        AVG(duration_seconds) OVER(PARTITION BY max_level_reached) AS avg_value
+    FROM Datawarehouse.gold.user_zscore_segmentation
 ),
 
 calc_stats AS (
@@ -20,8 +20,8 @@ calc_stats AS (
     SELECT 
         *,
         metric_value - avg_value AS diff,
-        POWER(metric_value - avg_value, 2) AS diff_sq,
-        POWER(metric_value - avg_value, 3) AS diff_cb,
+		CAST(POWER(metric_value - avg_value, 2) AS FLOAT) AS diff_sq,
+		CAST(POWER(metric_value - avg_value, 3) AS FLOAT) AS diff_cb,
         ROW_NUMBER() OVER(PARTITION BY grouping_id ORDER BY metric_value) AS row_num
     FROM base_data
 ),
@@ -39,7 +39,7 @@ summary_stats AS (
         -- Sum of cubes for Fisher's Skewness
         SUM(diff_cb) AS sum_diff_cb
     FROM calc_stats
-    GROUP BY 1, 2, 3
+    GROUP BY grouping_id, total_count, avg_value
 ),
 
 median_lookup AS (
@@ -48,7 +48,7 @@ median_lookup AS (
         grouping_id,
         metric_value AS median_value
     FROM calc_stats
-    WHERE row_num = CEIL(total_count / 2.0)
+    WHERE row_num = CEILiNG(total_count / 2.0)
 )
 
 --- Final Output ---
@@ -58,7 +58,7 @@ SELECT
     s.avg_value,
     m.median_value,
     s.std_dev,
-    -- Efficiency Ratio: Custom marketing metric
+    -- Efficiency Ratio: Custom marketing metric next/sqt avg * current
     (LEAD(s.total_count) OVER (ORDER BY s.grouping_id) / NULLIF(POWER(s.avg_value, 0.5) * POWER(s.total_count, 0.5), 0)) AS efficiency_index,
     -- Pearson’s Skewness: (Mean - Median) / StdDev
     CASE WHEN s.std_dev = 0 THEN 0 ELSE (3 * (s.avg_value - m.median_value)) / s.std_dev END AS skewness_pearson,
